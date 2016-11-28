6e0fe6777cee0d1f09bd5ef41740522eb4fa9142
Crazy4Tech <lihangtong@gmail.com>
2016-11-28 21:26:07 +0800

    add test

---
+++ src/main/java/com/developerx/httpclient/test001/Client.java
@@ -0,0 +1,13 @@
+package com.developerx.httpclient.test001;
+
+/**
+ * Hello world!
+ *
+ */
+public class Client 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/developerx/httpclient/test002/Client.java
@@ -0,0 +1,13 @@
+package com.developerx.httpclient.test002;
+
+/**
+ * Hello world!
+ *
+ */
+public class Client 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
